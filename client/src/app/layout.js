a1f1827632efe9995edf20a36e09adeb6e4dac1a
LayasaranP <d8972540@gmail.comProfile imaged8972540@gmail.com>
2026-01-14 18:29:37 +0530

    added client server folder

---
+++ client/src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DocMind",
+  description: "Agentic RAG SaaS platform: Build multi-tenant customer support chatbots + AI learning tools (flashcards, flowcharts, assessments, deep research) from uploaded documents.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      DocMind
+    </div>
+  )
+}
+
+export default page
